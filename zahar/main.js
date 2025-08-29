87795b52aa0558446ad6ad86b6c1c2fbb9f296f6
VladOS63K <119122824+VladOS63K@users.noreply.github.com>
2025-08-29 19:49:22 +0300

    image render changed

--- zahar/main.js
+++ zahar/main.js
@@ -1,27 +1,3 @@
-var test_key;
-var test_value;
-var test_title;
-var test_writebtn;
-var test_readbtn;
-
 document.addEventListener('DOMContentLoaded',function() {
-    test_key = document.getElementById('test-cloud-key');
-    test_value = document.getElementById('test-cloud-value');
-    test_title = document.getElementById('test-cloud-title');
-    test_writebtn = document.getElementById('write');
-    test_readbtn = document.getElementById('read');
-
-    test_writebtn.onclick = function() {
-        window.Telegram.WebApp.CloudStorage.setItem(test_key.value,test_value.value);
-    }
-    test_readbtn.onclick = function() {
-        window.Telegram.WebApp.CloudStorage.getItem(test_key.value,function(error,data){
-            if (error != null) {
-                test_title.innerHTML = error;
-            }
-            else {
-                test_title.innerHTML = data;
-            }
-        });
-    }
+    
 });
