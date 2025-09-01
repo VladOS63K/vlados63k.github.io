6f9cabf85c0c84d78c015cb4c4a2206ed40adc52
VladOS63K <119122824+VladOS63K@users.noreply.github.com>
2025-09-01 12:11:15 +0300

    Add files via upload

---
+++ scaM-forPC-ui/main.js
@@ -0,0 +1,33 @@
+var isRunning = false;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('button');
+    const settings = document.getElementById('settings');
+    const stateText = document.getElementById('state');
+
+    window.chrome.webview.addEventListener("message", function (e) {
+        if (e.data.Key == "state") {
+            isRunning = e.data.Value;
+            if (isRunning) {
+                stateText.innerHTML = "включен";
+            }
+            else {
+                stateText.innerHTML = "отключен";
+            }
+        }
+    });
+
+    button.onclick = function () {
+        window.chrome.webview.postMessage({
+            Key: "button_click",
+            Value: null
+        });
+    }
+
+    settings.onclick = function () {
+        window.chrome.webview.postMessage({
+            Key: "settings",
+            Value: null
+        });
+    }
+});
